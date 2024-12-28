b05d4b45d8b362f59251a6a590793ea596ee524e
Antony <antonis132011@hotmail.com>
2024-12-28 15:15:39 +0200

    move components into seperate file, implement inheritance logic.

---
+++ Spacetik/GameComponents.cpp
@@ -0,0 +1,211 @@
+#pragma once
+
+#include "graphics.h"
+#include <random>
+#include "vecmath.h"//an kanw sigkrouseis me ton tropo pou leei sto paradeigma
+#include "config.h"
+using namespace std;
+
+class GameComponent {
+public:
+	float center_x;
+	float center_y;
+	float speed;
+
+	GameComponent(float x, float y, float s) {
+		center_x = x;
+		center_y = y;
+		speed = s;
+	}
+
+	float get_center_x() {
+		return center_x;
+	}
+	float get_center_y() {
+		return center_y;
+	}
+	float get_speed() {
+		return speed;
+	}
+
+	// setters
+	void set_center_x(float center_x) {
+		this->center_x = center_x;
+	}
+	void set_center_y(float center_y) {
+		this->center_y = center_y;
+	}
+	void set_speed(float cs) {
+		speed = cs;
+	}
+};
+
+
+class Ball: public GameComponent {
+private:
+
+	float ball_direction_x;
+	float ball_direction_y;
+	float ball_radius;
+	float speed;
+
+
+	float init_ball_center_x;
+	float init_ball_center_y;
+	float init_ball_direction_x;
+	float init_ball_direction_y;
+
+public:
+	Ball(float x, float y, float dx, float dy, float br, float bs):
+		GameComponent(x,y,bs){ //default constructor
+		init_ball_center_x = x;
+		init_ball_center_y = y;
+		init_ball_direction_x = dx;
+		init_ball_direction_y = dy;
+
+		ball_radius = br;
+		startAgain();
+	}
+
+	void startAgain() { //gets called in the start or every time the ball touches the sides
+		center_x = init_ball_center_x;
+		center_y = init_ball_center_y;
+
+		// generates random numbers between -0.9999 to 0.9999
+		random_device seed;
+		default_random_engine generator(seed());
+		uniform_real_distribution <float> distribution(-1.0, 1.0);
+		float dx = distribution(generator);
+		float dy = distribution(generator);
+
+		// prokeimenou na min kanei polles anapidiseis sta plagia 
+		while (dy < 0.3 && dy > -0.3) dy = distribution(generator);
+		while (dx < 0.3 && dx > -0.3) dx = distribution(generator);
+		ball_direction_x = dx;
+		ball_direction_y = dy;
+
+	}
+
+	float get_ball_radius() {
+		return ball_radius;
+	}
+	float get_ball_direction_x() {
+		return ball_direction_x;
+	}
+	float get_ball_direction_y() {
+		return ball_direction_y;
+	}
+
+
+	void set_ball_direction_x(float ball_direction_x) {
+		this->ball_direction_x = ball_direction_x;
+	}
+	void set_ball_direction_y(float ball_direction_y) {
+		this->ball_direction_y = ball_direction_y;
+	}
+	void set_ball_radius(float br) {
+		ball_radius = br;
+	}
+	void set_init_ball_direction_y(float dy) {
+		init_ball_direction_y = dy;
+	}
+	void set_init_ball_direction_x(float dx) {
+		init_ball_direction_x = dx;
+	}
+
+};
+
+
+class Bar : public GameComponent{
+public:
+	float width;
+	float height;
+	int points;
+
+	Bar(float x, float y, float w, float h, float sp):
+		GameComponent(x,y,sp){
+		width = w;
+		height = h;
+		points = 0;
+	}
+
+	void moveup() {
+		center_y = center_y - speed;
+	}
+
+	void movedown() {
+		center_y = center_y + speed;
+	}
+
+	bool collisionWithBall(Ball& ball) {
+		if (abs(center_x - ball.get_center_x()) - (width / 2) - (ball.get_ball_radius() - 1) < 0) {
+			if (abs(center_y - ball.get_center_y()) - (height / 2) - (ball.get_ball_radius() - 1) < 0) {
+				return true; //egine sigrousi me tin mpala
+			}
+		}
+		return false;
+	}
+
+	float get_points() {
+		return points;
+	}
+	float get_width() {
+		return width;
+	}
+	float get_height() {
+		return height;
+	}
+
+};
+
+
+class Game {
+public:
+	int window_width;
+	int window_height;
+	int canvas_width;
+	int canvas_height;
+	int bounces;
+
+	Ball& ball;
+	Bar& player1;
+	Bar& player2;
+
+	Game(int ww, int wh, int cw, int ch, Ball& ball, Bar& p1, Bar& p2) :
+		ball(ball), player1(p1), player2(p2) {
+		window_width = ww;
+		window_height = wh;
+		canvas_width = cw;
+		canvas_height = ch;
+		bounces = 0;
+	}
+
+	static void drawBall() {
+
+		Game* game = reinterpret_cast <Game*>(graphics::getUserData()); // get all game data
+		graphics::Brush br;
+		br.fill_opacity = 0.95f;
+		br.outline_opacity = 0.0f;
+		br.texture = assets_path + "sun3.png";
+		graphics::drawDisk(game->ball.get_center_x(), game->ball.get_center_y(), game->ball.get_ball_radius(), br);
+	}
+	static void drawPlayers() {
+		Game* game = reinterpret_cast <Game*>(graphics::getUserData());
+		graphics::Brush br;
+		br.fill_opacity = 0.95f;
+		br.outline_opacity = 0.2f;
+		br.texture = assets_path + "bar3.png";
+		graphics::drawRect(game->player1.center_x, game->player1.center_y, game->player1.width, game->player1.height, br);
+		graphics::drawRect(game->player2.center_x, game->player2.center_y, game->player2.width, game->player2.height, br);
+	}
+	static void drawText() {
+		Game* game = reinterpret_cast <Game*>(graphics::getUserData());
+		graphics::Brush br;
+		br.fill_secondary_color[0] = 1.0f;
+		br.fill_secondary_color[1] = 1.0f;
+		br.fill_secondary_color[2] = 1.0f;
+		graphics::drawText(2, 5, 5, "P1:" + to_string(game->player1.points), br);
+		graphics::drawText(game->canvas_width - 13, 5, 5, "  P2:" + to_string(game->player2.points), br);
+	}
+
+};
--- Spacetik/Spacetik.cpp
+++ Spacetik/Spacetik.cpp
@@ -2,215 +2,19 @@
 #include <random>
 #include <string>
 #include <iostream>
-#include <cmath> //gia to apolyto abs
+#include <cmath>
+#include "GameComponents.cpp"
 
 #include "graphics.h"
-#include "vecmath.h"//an kanw sigkrouseis me ton tropo pou leei sto paradeigma
+#include "vecmath.h"	//an kanw sigkrouseis me ton tropo pou leei sto paradeigma
 #include "config.h"
 using namespace std;
-
-
-
-
-class Ball {
-private:
-	float ball_center_x;
-	float ball_center_y;
-	float ball_direction_x;
-	float ball_direction_y;
-	float ball_radius;
-	float ball_speed;
-
-
-	float init_ball_center_x;
-	float init_ball_center_y;
-	float init_ball_direction_x;
-	float init_ball_direction_y;
-public:
-	Ball(float x, float y, float dx, float dy, float br, float bs) { //default constructor
-		init_ball_center_x = x;
-		init_ball_center_y = y;
-		init_ball_direction_x = dx;
-		init_ball_direction_y = dy;
-
-		ball_radius = br;
-		ball_speed = bs;
-		startAgain();
-	}
-
-	void startAgain() { //gets called in the start or every time the ball touches the sides
-		ball_center_x = init_ball_center_x;
-		ball_center_y = init_ball_center_y;
-
-		// generates random numbers between -0.9999 to 0.9999
-		random_device seed;
-		default_random_engine generator(seed());
-		uniform_real_distribution <float> distribution(-1.0, 1.0);
-		float dx = distribution(generator);
-		float dy = distribution(generator);
-
-		// prokeimenou na min kanei polles anapidiseis sta plagia 
-		while (dy < 0.3 && dy > -0.3) dy = distribution(generator);
-		while (dx < 0.3 && dx > -0.3) dx = distribution(generator);
-		ball_direction_x = dx;
-		ball_direction_y = dy;
-
-	}
-
-	//getters
-	float get_ball_center_x() {
-		return ball_center_x;
-	}
-	float get_ball_center_y() {
-		return ball_center_y;
-	}
-	float get_ball_speed() {
-		return ball_speed;
-	}
-	float get_ball_radius() {
-		return ball_radius;
-	}
-	float get_ball_direction_x() {
-		return ball_direction_x;
-	}
-	float get_ball_direction_y() {
-		return ball_direction_y;
-	}
-	//set
-	void set_ball_center_x(float ball_center_x) {
-		this->ball_center_x = ball_center_x;
-	}
-	void set_ball_center_y(float ball_center_y) {
-		this->ball_center_y = ball_center_y;
-	}
-	void set_ball_speed(float bs) {
-		ball_speed = bs;
-	}
-	void set_ball_direction_x(float ball_direction_x) {
-		this->ball_direction_x = ball_direction_x;
-	}
-	void set_ball_direction_y(float ball_direction_y) {
-		this->ball_direction_y = ball_direction_y;
-	}
-	void set_ball_radius(float br) {
-		ball_radius = br;
-	}
-	void set_init_ball_direction_y(float dy) {
-		init_ball_direction_y = dy;
-	}
-	void set_init_ball_direction_x(float dx) {
-		init_ball_direction_x = dx;
-	}
-
-};
-
-
-class Bar {
-public:
-	float center_x;
-	float center_y;
-	float width;
-	float height;
-	float speed;
-	int points;
-
-	Bar(float x, float y, float w, float h, float sp) {
-		center_x = x;
-		center_y = y;
-		width = w;
-		height = h;
-		speed = sp;
-		points = 0;
-
-	}
-
-	void moveup() {
-		center_y = center_y - speed;
-	}
-
-	void movedown() {
-		center_y = center_y + speed;
-	}
-
-	bool collisionWithBall(Ball& ball) {
-		if (abs(center_x - ball.get_ball_center_x()) - (width / 2) - (ball.get_ball_radius() - 1) < 0) {
-			if (abs(center_y - ball.get_ball_center_y()) - (height / 2) - (ball.get_ball_radius() - 1) < 0) {
-				return true; //egine sigrousi me tin mpala
-			}
-		}
-		return false;
-	}
-
-	float get_points() {
-		return points;
-	}
-	float get_width() {
-		return width;
-	}
-	float get_height() {
-		return height;
-	}
-
-};
-
-
-class Game {
-public:
-	int window_width;
-	int window_height;
-	int canvas_width;
-	int canvas_height;
-	int bounces;
-
-	Ball& ball;
-	Bar& player1;
-	Bar& player2;
-
-	Game(int ww, int wh, int cw, int ch, Ball& ball, Bar& p1, Bar& p2) :
-		ball(ball), player1(p1), player2(p2) {
-		window_width = ww;
-		window_height = wh;
-		canvas_width = cw;
-		canvas_height = ch;
-		bounces = 0;
-	}
-
-	static void drawBall() {
-
-		Game* game = reinterpret_cast <Game*>(graphics::getUserData()); //pairneis ola ta stoixeia toy paixnidiou
-		graphics::Brush br;
-		br.fill_opacity = 0.95f;
-		br.outline_opacity = 0.0f;
-		br.texture = assets_path + "sun3.png";
-		graphics::drawDisk(game->ball.get_ball_center_x(), game->ball.get_ball_center_y(), game->ball.get_ball_radius(), br);
-	}
-	static void drawPlayers() {
-		Game* game = reinterpret_cast <Game*>(graphics::getUserData());
-		graphics::Brush br;
-		br.fill_opacity = 0.95f;
-		br.outline_opacity = 0.2f;
-		br.texture = assets_path + "bar3.png";
-		graphics::drawRect(game->player1.center_x, game->player1.center_y, game->player1.width, game->player1.height, br);
-		graphics::drawRect(game->player2.center_x, game->player2.center_y, game->player2.width, game->player2.height, br);
-	}
-	static void drawText() {
-		Game* game = reinterpret_cast <Game*>(graphics::getUserData());
-		graphics::Brush br;
-		br.fill_secondary_color[0] = 1.0f;
-		br.fill_secondary_color[1] = 1.0f;
-		br.fill_secondary_color[2] = 1.0f;
-		graphics::drawText(2, 5, 5, "P1:" + to_string(game->player1.points), br);
-		graphics::drawText(game->canvas_width - 13, 5, 5, "  P2:" + to_string(game->player2.points), br);
-	}
-
-};
 
 
 
 void draw() {
 	graphics::Brush br;
 	br.texture = assets_path + "galaxy3.png"; // analogia 2 pros tria peripou
-	std::cout << "Galaxy path: " << assets_path + "galaxy3.png" << std::endl;
 	br.outline_opacity = 0.0f;
 
 	graphics::drawRect(CANVAS_HEIGHT / 2, CANVAS_WIDTH / 2, CANVAS_WIDTH + 150, CANVAS_WIDTH, br);
@@ -252,24 +56,24 @@ void update(float ms) {
 	bool play_collision_sound = false;
 	Ball& ball = game->ball;
 	//gia tin mpala
-	if ((ball.get_ball_center_x() + ball.get_ball_radius()) >= game->canvas_width) { //xtipise deksia ara pontos
+	if ((ball.get_center_x() + ball.get_ball_radius()) >= game->canvas_width) { //xtipise deksia ara pontos
 		std::string wav = assets_path + "point2.wav";
 		graphics::playSound(wav, 0.5f);
 		player1.points++;
 		ball.startAgain();
 	}
-	else if ((ball.get_ball_center_x() - ball.get_ball_radius()) <= 0.0f) { //xtipise aristera ara pontos
+	else if ((ball.get_center_x() - ball.get_ball_radius()) <= 0.0f) { //xtipise aristera ara pontos
 		std::string wav = assets_path + "point2.wav";
 		graphics::playSound(wav, 0.6f);
 		player2.points++;
 		ball.startAgain();
 	}
-	else if ((ball.get_ball_center_y() + ball.get_ball_radius()) >= game->canvas_height) { // xtipise pano 
+	else if ((ball.get_center_y() + ball.get_ball_radius()) >= game->canvas_height) { // xtipise pano 
 		//kala tha itan na einai me tin epomeni thesi
 		ball.set_ball_direction_y(-1 * ball.get_ball_direction_y());
 		play_collision_sound = true;
 	}
-	else if ((ball.get_ball_center_y() - ball.get_ball_radius()) <= 0.0f) { // xtipise kato
+	else if ((ball.get_center_y() - ball.get_ball_radius()) <= 0.0f) { // xtipise kato
 		ball.set_ball_direction_y(-1 * ball.get_ball_direction_y());
 		play_collision_sound = true;
 	}
@@ -287,8 +91,8 @@ void update(float ms) {
 		game->bounces++;
 	}
 	//nea thesi gia tin mpala
-	ball.set_ball_center_x(ball.get_ball_center_x() + ball.get_ball_direction_x() * ball.get_ball_speed()); //+= ball.get_ball_direction_x() * ball.get_ball_speed();
-	ball.set_ball_center_y(ball.get_ball_center_y() + ball.get_ball_direction_y() * ball.get_ball_speed()); //+= ball.get_ball_direction_y() * ball.get_ball_speed();
+	ball.set_center_x(ball.get_center_x() + ball.get_ball_direction_x() * ball.get_speed()); //+= ball.get_ball_direction_x() * ball.get_ball_speed();
+	ball.set_center_y(ball.get_center_y() + ball.get_ball_direction_y() * ball.get_speed()); //+= ball.get_ball_direction_y() * ball.get_ball_speed();
 }
 
 
